04af48c33f2ee28f4a5c662ada2fbe8842f06f61
stiven medina <69610934+yorsthiven@users.noreply.github.com>
2025-10-01 15:02:30 -0500

    Inicializando proyecto
    instalacion de angular/firebase, creacion de enviroment. conexion a Firebase, Creacion de primer servicio,

---
+++ daily-dashboard/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { environment } from './../enviroments/enviroment';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+// import { environment } from '../enviroments/environment';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()), // <-- aquí Firestore
+
+  ]
+};
---
+++ daily-dashboard/src/enviroments/enviroment.ts
@@ -0,0 +1,36 @@
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "tu_api_key",
+//     authDomain: "tu_auth_domain",
+//     projectId: "tu_project_id",
+//     storageBucket: "tu_storage_bucket",
+//     messagingSenderId: "tu_sender_id",
+//     appId: "tu_app_id"
+//   }
+// };
+
+
+// Your web app's Firebase configuration
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDaFqxbhfkOuST8s6iaj4jIYsP7nlp3fC8",
+//   authDomain: "daily-dashboard-26f07.firebaseapp.com",
+//   projectId: "daily-dashboard-26f07",
+//   storageBucket: "daily-dashboard-26f07.firebasestorage.app",
+//   messagingSenderId: "812892785253",
+//   appId: "1:812892785253:web:10cbbc62616ef0123ad48c"
+// };
+
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyDaFqxbhfkOuST8s6iaj4jIYsP7nlp3fC8",
+  authDomain: "daily-dashboard-26f07.firebaseapp.com",
+  projectId: "daily-dashboard-26f07",
+  storageBucket: "daily-dashboard-26f07.firebasestorage.app",
+  messagingSenderId: "812892785253",
+  appId: "1:812892785253:web:10cbbc62616ef0123ad48c"
+}
+};
+
